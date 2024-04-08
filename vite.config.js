31d26befc9301d769c19b50baee0cf1568dcef33
= <lycaon.anderson@gmail.com>
2024-04-09 02:31:02 +0800

    Update homepage URL in package.json and set base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/flowmodoro/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/flowmodoro/",
 });
